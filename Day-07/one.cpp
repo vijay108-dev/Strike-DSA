c7bd28f18191273a3adf643d2cf9c7c9060b181d
vijay108-dev <vijay108.dev@gmail.com>
2025-12-21 10:44:59 +0530

    Add: Day-07: Introduction to Array

---
+++ Day-07/one.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//     An array is a collection of elements of the same data type stored in continuous memory locations.
+// Each element is accessed using an index, starting from 0.
+
+
+// We need an array to store multiple values of the same type using a single variable name.
+// It makes data management and processing easier, faster, and more organized.
+
+// int marks[5]={66,89,90,60,45};
+// // cout<<marks[0]<<endl;
+// // cout<<marks[1]<<endl;
+// // cout<<marks[2]<<endl;
+// // cout<<marks[3]<<endl;
+// // cout<<marks[4]<<endl;
+// for(int i=0;i<5;i++){
+//     cout<<marks[i]<<endl;
+// }
+
+
+    ////  int n;
+    //// cout<<"Tell me the size of an array\n";
+    //// cin>>n;
+    
+
+    // int arr[5];
+
+    // cout<<"Enter the value in array";
+    // for(int i=0;i<5;i++){
+    //     cin>>arr[i];
+    // }
+
+
+    // for(int i=0;i<5;i++){
+    //     cout<<arr[i]<<endl;
+    // }
+
+
+
+
+// int a=12;
+// int b=13;
+
+// b=23;
+// cout<<&a<<endl;//0x16b43ec98
+// cout<<&b<<endl;//0x16b43ec94
+
+
+//we store the address of first one and another we will get on the fly
+
+
+
+
+// int marks[5]={56,78,90,87,90};
+// int sum=0;
+// for(int i=0;i<5;i++){
+//     sum+=marks[i];
+// }
+// cout<<sum<<endl;
+
+
+
+
+
+// int marks[5]={56,78,90,87,90};
+// for(int i=0;i<5;i++){
+//     cout<<&marks[i]<<endl;
+// }
+
+
+
+
+
+// int marks[5]={56,78,50,87,90};
+// int heighest=marks[0];
+// int index=0;
+// for(int i=1;i<5;i++){
+//     if(marks[i]>heighest)
+//     heighest=marks[i];
+//     index=i;
+
+// }
+// cout<<heighest<<endl<<index<<endl;
+
+
+
+
+
+// int marks[5]={56,78,50,87,90};
+// int x;
+// cout<<"Enter the value of x"<<endl;
+// cin>>x;
+// for(int i=0;i<5;i++){
+//     if(marks[i]==x){
+//         cout<<"got it"<<endl;
+//         return 0;
+//     }
+// }
+//   cout<<"not persent"<<endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+}
