bc236f05f38938f6f2f4d06627ac3f665334d780
vijay108-dev <vijay108.dev@gmail.com>
2026-01-01 14:52:11 +0530

    Add: Day-11: Time and space Complexity Part-2

---
+++ Day-11/one.cpp
@@ -0,0 +1,80 @@
+
+// Program
+
+// for(int i=0;i<n;i++){
+//     cout<<"Hello";
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//         cout<<"Hello";
+//     }
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<=i;j++){
+//         cout<<"Hello";
+//     }
+// }
+
+
+// for(int i=0;i<n;i=i+2){
+//     cout<<"Hello";
+// }
+
+
+// for(int i=0;i<n;i=i*2){
+//     cout<<"Hello";
+// }
+
+// for(int i=0;i<n;i=i*3){
+//     cout<<"Hello";
+// }
+
+// for(int i=n;i>0;i=i/2){
+//     cout<<"Hello";
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//         for(int k=0;k<n;k++){
+//             cout<<"Hello";
+//         }
+//     }
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<i*i;j++){
+//         cout<<"Hello";
+//     }
+// }
+
+
+
+
+// n
+
+// for(int i=0;i<n;i++){
+//    for(int j=0;j<n;j++){
+//     cout<<"Hello";
+//    }
+// }
+
+
+// for(int i=0;i<n;i++){
+   
+//     //  n operation
+// }
+
+
+// for(int i=0;i<n;i++){
+//     for(j=0;j<=i;j++){
+//         cout<<"Hello";
+//     }
+// }
+
+// i = 0        i=1          i=2         i=n-1
+// j = 1 time.  j= 2 time.   j=3 time.   j= n time
+   
+
+// 1+ 2 +3+ ... n = n*(n+1)/2 = n^2
